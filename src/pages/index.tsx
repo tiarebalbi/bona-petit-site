04ffd003a67c56f1f73758bf25d9aacacae826a0
Tiarê Balbi <me@tiarebalbi.com>
2023-04-27 21:34:00 -0700

    Chore: adding mobile support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,19 @@ import image from '../../public/logo-black.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-12 sm:p-24">
       <Head>
         <title>Bona Petit - Site em Manutenção</title>
       </Head>
-      <Image src={image} alt="Bona Petit" className="w-1/3" />
-      <h1 className="text-[48px] font-medium mb-4">Site em manutenção</h1>
-      <p className="text-gray-950/80 w-1/3 text-center">
+      <Image
+        src={image}
+        alt="Bona Petit"
+        className="w-[90vw] md:w-[500px] mb-12"
+      />
+      <h1 className="text-[48px] font-medium mb-4 text-center">
+        Site em manutenção
+      </h1>
+      <p className="text-gray-950/80 w-[80vw] md:w-2/3 lg:w-1/3 text-center">
         Desculpe-nos pelo transtorno, o site está em manutenção no momento, mas
         estará de volta em breve.
       </p>
