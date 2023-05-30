0c8b39a482f3c011a5ad66c7b626fdbf093eedc4
Tiarê Balbi <tiarebalbi@users.noreply.github.com>
2023-05-30 00:28:06 +0000

    Footer setup

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -1,3 +1,14 @@
-const Footer = () => <footer>Footer</footer>;
+import style from './style.module.css';
+
+const Footer = () => (
+  <footer className={style.main}>
+    <div className={style.faleConosco}>
+      <div>Fale Conosco</div>
+      <div>Form</div>
+    </div>
+    <div className={style.parceiros} />
+    <div className={style.overlay} />
+  </footer>
+);
 
 export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-export const metadata = {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
   title: 'Bona Petit: Página Inícial',
   description:
     'Descubra a Bona Petit, casa dos doces mais requintados e inovadores de São José dos Campos, comandada pelo chef e fundador, um mestre na arte da confeitaria. Embarque na sua jornada culinária, explore o revolucionário Método Milenar de Confeitaria e descubra a BP Class, nossa plataforma de confeitaria online que educa milhares de alunos ao redor do mundo. Independentemente de onde você esteja na sua aventura culinária, estamos aqui para apoiá-lo. Bem-vindo à Bona Petit, onde a confeitaria encontra a paixão e a inovação.',
+  keywords: [
+    'Bona Petit',
+    'Chef Executivo',
+    'Método Milenar de Confeitaria',
+    'BP Class',
+    'Confeitaria online',
+    'Empreendedorismo na confeitaria',
+    'Receitas lucrativas',
+    'Produção em grande escala de doces',
+    'Confeitaria na Itália, Espanha e França',
+    'Bona Petit São José dos Campos',
+    'Experiência do chef',
+    'Ensino de confeitaria',
+    'Alcançar objetivos na confeitaria',
+    'Educação em confeitaria',
+    'Crescimento na confeitaria',
+  ],
 };
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hi there
-    </main>
-  );
+  return <main>Hi there</main>;
 }
