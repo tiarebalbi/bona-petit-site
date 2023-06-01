6e2838cacf932e3280877534a47cff139c92dcc3
Tiarê Balbi <me@tiarebalbi.com>
2023-06-01 10:03:37 -0700

    Chore: fixing mobile for feedback

--- src/components/feedback.tsx
+++ src/components/feedback.tsx
@@ -11,10 +11,10 @@ export interface FeedbackProps {
 
 const Feedback: React.FC<FeedbackProps> = ({ data }) => {
   return (
-    <div>
-      <Rate disabled defaultValue={data.ranking} />
+    <div className="flex items-center px-12 lg:px-0 lg:items-start justify-center lg:justify-start flex-col w-full">
+      <Rate count={data.ranking} disabled defaultValue={data.ranking} />
       <p className="italic my-2 prose text-primary-100">{data.message}</p>
-      <div className="flex items-center justify-start space-x-4 mt-8">
+      <div className="flex items-center justify-center lg:justify-start space-x-4 mt-8">
         <div className="w-[36px] h-[36px] bg-white rounded-full" />
         <span className="font-medium">{data.name}</span>
       </div>
